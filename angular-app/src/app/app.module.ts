aa97cdbc1ffae0a4010e5e33d530f3fa99e0ce15
Andrés Sierra <andres.sierra@beitech.co>
2017-07-07 16:42:26 -0500

    Quitar archivos no necesarios

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -5,25 +5,17 @@ import { RouterModule } from '@angular/router';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroSearchComponent } from './hero-search.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroService } from './hero.service';
 
 import {FrutaComponent} from '../app/fruta/fruta.component';
 import {EmpleadoComponent} from '../app/empleado/empleado.component'
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent,
-    HeroesComponent,
-    HeroSearchComponent,
     FrutaComponent, 
     EmpleadoComponent
   ],
@@ -31,13 +23,11 @@ import { InMemoryDataService }  from './in-memory-data.service';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule,
-    InMemoryWebApiModule.forRoot(InMemoryDataService)
+    HttpModule
     
   ],
 
   providers: [
-    HeroService
   ],
   bootstrap: [AppComponent]
 })
